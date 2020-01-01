f1d0f3f33607de20d313ecc7048a88d2dd54c967
ANGELLOPARR <angellop123@gmail.com>
2019-12-31 16:41:37 -0800

    life cycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,12 @@ export class AppComponent {
       content: blueprintData.blueprintContent
     });
   }
+
+  onChangeFirst() {
+    this.serverElements[0].name="newly changed name."
+  }
+
+  onDeleteFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
--- src/app/section5/server-element/server-element.component.ts
+++ src/app/section5/server-element/server-element.component.ts
@@ -1,17 +1,69 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   // argument given to @Input decorator is the alias for the property name
   @Input('srvElement') element: {type: string, name: "string", }
+  @Input() name: string;
 
-  constructor() { }
+  constructor() { 
+    console.log("constructor called");
+  }
 
   ngOnInit() {
+    console.log("ngOnInit called");
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges called");
+    console.log(changes);
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck called");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit called");
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked called");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit called");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked called");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy called");
   }
 
 }
